646e847ea9577e09b02de98265c020f3175e27cb
k2tzumi <katzumi+github@gmail.com>
2020-01-25 09:46:35 +0900

    Change target es2018

--- src/Code.ts
+++ src/Code.ts
@@ -2,13 +2,13 @@ const properties = PropertiesService.getScriptProperties();
 const VERIFICATION_TOKEN: string = properties.getProperty('VERIFICATION_TOKEN')
 
 function doPost(e): GoogleAppsScript.Content.TextOutput {
-  var postData = JSON.parse(e.postData.getDataAsString())
+  let postData = JSON.parse(e.postData.getDataAsString())
   if (postData.token !== VERIFICATION_TOKEN) {
     console.warn('Invalid verification token: %s', postData.token)
     throw new Error('Invalid verification token.')
   }
 
-  var res = {}
+  let res = {}
   switch (postData.type) {
     case 'url_verification':
       console.log({ message: 'url_verification called.', data: postData.challenge })
@@ -45,12 +45,12 @@ const NOTIFICATION_MESSAGE: string = properties.getProperty('NOTIFICATION_MESSAG
 
 function emojiChanged(event: string[]) {
   if (event['subtype'] === 'add') {
-    var emoji_name = event["name"]
+    let emoji_name = event["name"]
     console.log(emoji_name)
-    var message = `${NOTIFICATION_MESSAGE} :${emoji_name}: \`:${emoji_name}:\``
+    let message = `${NOTIFICATION_MESSAGE} :${emoji_name}: \`:${emoji_name}:\``
 
-    if (event['value'].indexOf('alias:') === 0) {
-      var origin_emoji = event['value'].replace(/^alias:/, '')
+    if (event['value'].startsWith('alias:')) {
+      let origin_emoji = event['value'].replace(/^alias:/, '')
       message += ` (alias of \`:${origin_emoji}:\`)`
     }
     postSlack(message)
@@ -62,8 +62,8 @@ function emojiChanged(event: string[]) {
 }
 
 function eventIdProceeded(eventId: string): boolean {
-  var cash = CacheService.getScriptCache();
-  var prevEventId = cash.get(eventId);
+  let cash = CacheService.getScriptCache();
+  let prevEventId = cash.get(eventId);
   if (prevEventId) {
     return true
   } else {
@@ -77,14 +77,14 @@ const USER_NAME: string = 'new_emoji_bot'
 const ICON: string = ':chbimal_chick:'
 
 function postSlack(message: string): void {
-  var jsonData =
+  let jsonData =
   {
     "username": USER_NAME,
     "icon_emoji": ICON,
     "text": message
   }
 
-  var options: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions =
+  let options: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions =
   {
     "method": "post",
     "contentType": "application/json",
